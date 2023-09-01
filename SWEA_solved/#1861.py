1b9d6deb3e4e24b9aa3422ef62ef1edf0865147e
Seon Wook Gim <tjsdnr960@naver.com>
2023-09-01 17:10:45 +0900

    #1861 정사각형 방

---
+++ SWEA_solved/#1861.py
@@ -0,0 +1,35 @@
+def search(start):
+    global N
+    dy = [-1, 1, 0, 0]
+    dx = [0, 0, -1, 1]
+    cnt = 0
+    stack = [start]
+    while stack:
+        x, y = stack.pop()
+        cnt += 1
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            if 0 <= nx < N and 0 <= ny < N:
+                if arr[x][y] + 1 == arr[nx][ny]:
+                    stack.append([nx, ny])
+    return cnt
+
+
+
+for test_case in range(1, int(input()) + 1):
+    N = int(input())
+    arr = list(list(map(int, input().split())) for _ in range(N))
+    answer = [float('inf'), 0]
+
+    for i in range(N):
+        for j in range(N):
+            cnt = search((i, j))
+            if cnt > answer[1]:
+                answer[1] = cnt
+                answer[0] = arr[i][j]
+            elif cnt == answer[1]:
+                if arr[i][j] < answer[0]:
+                    answer[0] = arr[i][j]
+
+    print(f'#{test_case} {answer[0]} {answer[1]}')
